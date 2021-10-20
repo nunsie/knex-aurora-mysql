bf6879856eaaad0ebdc87fba41a35f80468136a8
Nusrath Khan <nusrath501khan@gmail.com>
2021-10-20 18:35:13 +0200

    fix: remove overrides

--- index.js
+++ index.js
@@ -1,22 +1,11 @@
+const knexDataApiClient = require('knex-aurora-data-api-client');
 const Bluebird = require('bluebird');
-const MySqlClient = require('knex/lib/dialects/mysql');
-const dataApi = require('knex-aurora-data-api-client/src/data-api');
-const constants = require('knex-aurora-data-api-client/src/constants');
 
-// Call mysql client to setup knex, this set as this function
-const client = MySqlClient.constructor
-  ? class MysqlClientRDSDataAPI extends MySqlClient {}
-  : function MysqlClientRDSDataAPI(config) {
-      MySqlClient.call(this, config);
-    };
-
-Object.assign(client.prototype, {
+Object.assign(knexDataApiClient.mysql.prototype, {
   acquireConnection() {
     const connection = this._driver(this.connectionSettings);
     return Bluebird.resolve(connection);
   },
 });
 
-dataApi(client, MySqlClient, constants.dialects.mysql);
-
-module.exports = client;
+module.exports = knexDataApiClient.mysql;
